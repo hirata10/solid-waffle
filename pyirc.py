99eaa333e26a81f547c981934f225e566d168777
Christopher Hirata <hirata.10@osu.edu>
2017-08-07 12:56:37 -0400

    Changed color bar outputs

--- pyirc.py
+++ pyirc.py
@@ -682,3 +682,10 @@ def hotpix(darkfiles, formatpars, tslices, pars, verbose):
   if verbose: print numpy.sum(this_hot)
 
   return numpy.where(this_hot>0)
+
+# Generates min and max range for a color bar
+# based on inter-quartile range
+def get_vmin_vmax(mydata, qext):
+  Q1 = numpy.percentile(mydata,25)
+  Q2 = numpy.percentile(mydata,75)
+  return Q1-(Q2-Q1)*qext, Q2+(Q2-Q1)*qext
