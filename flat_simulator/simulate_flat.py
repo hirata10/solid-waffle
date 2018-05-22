dbe669c551d3e6a2a86e1c1661e5fbc9c867c8ca
Ami Choi <amichoi@gmail.com>
2018-05-22 10:20:03 -0400

    Saving some intermediate changes for floating point variables and time steps

--- flat_simulator/simulate_flat.py
+++ flat_simulator/simulate_flat.py
@@ -25,17 +25,23 @@ nx, ny = 4096, 4096
 #nx, ny = 128, 128
 tsamp = 66
 I = 2.0 # arbitrary scalar for now (e/s/pixel)
-delta_t = 3.0 # arbitrary for now (s)
+delta_tsamp = 3.0 # arbitrary for now (s)
+nt_step = tsamp*3 # number of tot timesteps depending on convergence needs
+delta_t = (delta_tsamp*tsamp)/nt_step # time between timesteps
+allQ = np.zeros((nt_step, nx, ny))
 data_cube_Q = np.zeros((tsamp, nx, ny))
 data_cube_S = np.zeros_like(data_cube_Q)
 gain = 1.5 # arbitrary scalar e-/DN
 
 # Start with 0 charge in the first frame (t=0)
-for tdx in range(1, tsamp):
+for tdx in range(1, nt_step):
     mean = I*delta_t
-    data_cube_Q[tdx,:,:] = data_cube_Q[tdx-1,:,:]+np.random.poisson(
-        mean, data_cube_Q[tdx,:,:].shape)
-    data_cube_S[tdx,:,:] =  data_cube_Q[tdx,:,:]/gain
+    # Create realization of charge
+    allQ[tdx,:,:] = alLQ[tdx-1,:,:]+np.random.poisson(
+        mean, allQ[tdx,:,:].shape)
+    #data_cube_Q[tdx,:,:] = data_cube_Q[tdx-1,:,:]+np.random.poisson(
+    #    mean, data_cube_Q[tdx,:,:].shape)
+    #data_cube_S[tdx,:,:] =  data_cube_Q[tdx,:,:]/gain
 
 # Open up an example DCL flat file and save the data cube
 dclfile = 'Set_001_Test_0002.fits'
@@ -51,7 +57,6 @@ fitsio.write(dclfile, data_cube_S, clobber=True)
 Things planned:
  * unsigned 16-bit integers
  * offset & clipping
- * I, dt -> floating point
  * reference pixels (4 around edge for all but WFC3 which has 5)
  * generate flats and darks
  * use real dark cube as read noise is reasonable, won't do hot pixels correctly, but ok for now
