31532c009ca3f7a3b3192972eafd56f55057e370
Ami Choi <amichoi@gmail.com>
2018-06-05 17:50:10 -0400

    Some changes related to reference pixels, speeding up the loop and reducing memory usage, and starting to use functions from pyirc.

--- flat_simulator/simulate_flat.py
+++ flat_simulator/simulate_flat.py
@@ -20,30 +20,44 @@ from numpy.random import randn,poisson
 import astropy.io.fits as fits
 import fitsio
 from fitsio import FITS,FITSHDR
+import sys
+sys.path.insert(0, '../')
+#sys.path.insert(0, '/users/PCON0003/cond0080/src/solid-waffle/')
+from pyirc import *
 
 # data cube attributes
+formatpars=1
 nx, ny = 4096, 4096
 #nx, ny = 128, 128
+N = get_nside(formatpars) # possibly redundant with nx,ny
+# Reference pixels hard-coded to 4 rows/cols around border, true for
+# all except WFC3 which has 5
+xmin,xmax,ymin,ymax = 4,N-4,4,N-4 # Extent of non-reference pixels
 tsamp = 66
 I = 2.0 # arbitrary scalar for now (e/s/pixel)
 delta_tsamp = 3.0 # arbitrary for now (s)
 nt_step = tsamp*3 # number of tot timesteps depending on convergence needs
 delta_t = (delta_tsamp*tsamp)/nt_step # time between timesteps
-allQ = np.zeros((nt_step, nx, ny))
+allQ = np.zeros((nt_step/tsamp, nx, ny))
 data_cube_Q = np.zeros((tsamp, nx, ny))
 data_cube_S = np.zeros_like(data_cube_Q)
 gain = 1.5 # arbitrary scalar e-/DN
+count = 1
 
 # Start with 0 charge in the first frame (t=0)
 for tdx in range(1, nt_step):
     mean = I*delta_t
     # Create realization of charge
-    # Seems to use up loads of memory, is there a better way?
-    allQ[tdx,:,:] = allQ[tdx-1,:,:]+np.random.poisson(
-        mean, allQ[tdx,:,:].shape)
-
-# Sample tsamp times
-data_cube_Q = allQ[::(nt_step/tsamp),:,:]
+    # This version uses less memory, but probably still sub-optimal
+    idx = tdx%(nt_step/tsamp)
+    allQ[idx,xmin:xmax,ymin:ymax] = allQ[idx-1,xmin:xmax,xmin:xmax] \
+        +np.random.poisson(mean, allQ[idx,xmin:xmax,xmin:xmax].shape)
+    if (idx==0):
+        data_cube_Q[count,:,:] = allQ[idx,:,:]
+        allQ = np.zeros((nt_step/tsamp, nx, ny))
+        allQ[0,:,:] = data_cube_Q[count,:,:]
+        count += 1
+# Convert charge to signal
 data_cube_S = np.array(data_cube_Q/gain, dtype=np.uint16)
 
 # Open up an example DCL flat file and save the data cube
@@ -59,7 +73,6 @@ fitsio.write(dclfile, data_cube_S, clobber=True)
 """
 Things planned:
  * offset & clipping
- * reference pixels (4 around edge for all but WFC3 which has 5)
  * generate flats and darks
  * use real dark cube as read noise is reasonable, won't do hot pixels correctly, but ok for now
  * multiply I by quantum efficiency 
