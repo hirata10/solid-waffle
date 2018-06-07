39e4423e34baf8d4555b76e61a4f5f7c0fede4af
Ami Choi <amichoi@gmail.com>
2018-06-07 10:04:35 -0400

    introduce quantum efficiency, just a number for now

--- flat_simulator/simulate_flat.py
+++ flat_simulator/simulate_flat.py
@@ -35,6 +35,7 @@ N = get_nside(formatpars) # possibly redundant with nx,ny
 xmin,xmax,ymin,ymax = 4,N-4,4,N-4 # Extent of non-reference pixels
 tsamp = 66
 I = 2.0 # arbitrary scalar for now (e/s/pixel)
+QE=0.8 # quantum efficiency, will eventually be a map
 delta_tsamp = 3.0 # arbitrary for now (s)
 nt_step = tsamp*3 # number of tot timesteps depending on convergence needs
 delta_t = (delta_tsamp*tsamp)/nt_step # time between timesteps
@@ -50,8 +51,9 @@ for tdx in range(1, nt_step):
     # Create realization of charge
     # This version uses less memory, but probably still sub-optimal
     idx = tdx%(nt_step/tsamp)
+    # Charge accumulates, dependent on quantum efficiency of the pixel
     allQ[idx,xmin:xmax,ymin:ymax] = allQ[idx-1,xmin:xmax,xmin:xmax] \
-        +np.random.poisson(mean, allQ[idx,xmin:xmax,xmin:xmax].shape)
+        +QE * np.random.poisson(mean, allQ[idx,xmin:xmax,xmin:xmax].shape)
     if (idx==0):
         data_cube_Q[count,:,:] = allQ[idx,:,:]
         allQ = np.zeros((nt_step/tsamp, nx, ny))
@@ -75,5 +77,4 @@ Things planned:
  * offset & clipping
  * generate flats and darks
  * use real dark cube as read noise is reasonable, won't do hot pixels correctly, but ok for now
- * multiply I by quantum efficiency 
 """
