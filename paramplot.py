7944bf59a451cd7bd71d02c8c93015f2a3660673
Jenna Freudenburg <cond0088@pitzer-rw01.ten.osc.edu>
2020-01-31 07:23:42 -0500

    update paramplot.py

--- paramplot.py
+++ paramplot.py
@@ -15,7 +15,7 @@ sim_ipnl = -1.1590
 sim_ipnlNN = 0.2034
 
 sim_truevals = [sim_alphaV,sim_alphaH,sim_beta2*sim_gain,
-		sim_beta3*sim_gain**2,sim_beta4*sim_gain**3,sim_gain,None,None]
+		sim_beta3*sim_gain**2,sim_beta4*sim_gain**3,sim_gain,sim_ipnl,sim_ipnlNN]
  
  
 def read_summary(fname):
@@ -31,8 +31,11 @@ def read_summary(fname):
             'ipnl(2,2)','t_intercept','beta2','beta3','beta4']
 
     table = pd.read_table(fname,delim_whitespace=True,index_col=(0,1),names=colnames,comment='#')
-    table['ipnlNN'] = (table['ipnl(0,1)']+table['ipnl(0,-1)']
+
+    # Adjust units as appropriate to this plot
+    table['ipnlNN'] = 1e6*(table['ipnl(0,1)']+table['ipnl(0,-1)']
                     + table['ipnl(1,0)']+table['ipnl(-1,0)'])/4.0
+    table['ipnl(0,0)'] *= 1e6
     table['alphaH'] *= 100
     table['alphaV'] *= 100
     table['alphaD'] *= 100
@@ -58,17 +61,18 @@ files = [prefix+'chris_20663st_summary.txt',
 	 prefix+'chris_20829st-short_summary.txt',
 	 prefix+'chris_20829st-med_summary.txt',
 	 prefix+'full_quart_nl_paperI_JG_pyircv25_16by16_summary.txt',
-	 prefix+'full_quart_nl_paperI_JG_pyircv25_32by32_summary.txt']
+	 prefix+'full_quart_nl_paperI_JG_pyircv25_32by32_summary.txt',
+	 prefix+'ACredo_JG_paperI_sims_adv_fullnl_summary.txt']
 
 # Set up figure
 ylabels = ['SCA 20663, fiducial','SCA 20663, 128x16','SCA 20663, cubic CNL','SCA 20663, lo (1 3 4 6)','SCA 20663, short (5 7 8 10)','SCA 20663, med (3 6 7 10)',
 	   'SCA 20828, fiducial','SCA 20828, 128x16','SCA 20828, cubic CNL','SCA 20828, lo (1 3 4 6)','SCA 20828, short (5 7 8 10)','SCA 20828, med (3 6 7 10)',
 	   'SCA 20829, fiducial','SCA 20829, 128x16','SCA 20829, cubic CNL','SCA 20829, lo (1 3 4 6)','SCA 20829, short (5 7 8 10)','SCA 20829, med (3 6 7 10)']
-sim_ylabels = ['simulations, 16x16','simulations, 32x32']
+sim_ylabels = ['simulations, 16x16','simulations, 32x32','simulations, redo']
 divisions = [5,11,17]
 axis_names = [r'$\alpha_V$',r'$\alpha_H$',
               r'$\beta_2g$',r'$\beta_3g^2$',r'$\beta_4g^3$',
-              r'$g$',r'$[K^2a+KK\']_{0,0}$',r'$[K^2a+KK\']_{NN}$']
+              r'$g$',r'$[K^2a+KK\']_{0,0}$',r'$[K^2a+KK\']_{<1,0>}$']
 units = ['%','%',r'DN$^{-1}$',r'DN$^{-2}$',r'DN$^{-3}$','e/DN',r'ppm/e',r'ppm/e']
 
 xdata_labels = ['alphaV','alphaH','beta2','beta3','beta4',
@@ -109,15 +113,16 @@ for i,ax in enumerate(axes):
     for j,run in enumerate(ylabels):
         mean = np.mean(tables[j][xdata_labels[i]])
         stdev = np.std(tables[j][xdata_labels[i]])
-        ax.errorbar([mean],[j],xerr=[stdev],capsize=8.0,markersize=10,marker='o',
+	error = stdev#/len(tables[j]**0.5)
+        ax.errorbar([mean],[j],xerr=[error],capsize=8.0,markersize=10,marker='o',
                     color=colors[j%(divisions[1]-divisions[0])])
         if j==0:
             for k, div in enumerate(divisions):
                 ax.axhline(y=div+0.5,color='k',linewidth=0.75,linestyle='-')
 
         if j%(divisions[1]-divisions[0])==0:
-            ax.fill_betweenx([j-0.5,j+divisions[1]-divisions[0]-0.5],[mean-stdev,mean-stdev],
-                             [mean+stdev,mean+stdev], color = 'grey', alpha = 0.5)
+            ax.fill_betweenx([j-0.5,j+divisions[1]-divisions[0]-0.5],[mean-error,mean-error],
+                             [mean+error,mean+error], color = 'grey', alpha = 0.5)
             
 sim_tables = tables[-2:]
 
@@ -127,7 +132,7 @@ for i,sax in enumerate(sim_axes):
     sax.set_yticklabels([])
     
     if i==0:
-        sax.set_yticks([i for i in range(len(sim_ylabels))])
+        sax.set_yticks([n for n in range(len(sim_ylabels))])
         sax.set_yticklabels(sim_ylabels)
         sax.set_ylim(len(sim_ylabels)-0.5,-0.5)
 
@@ -136,7 +141,8 @@ for i,sax in enumerate(sim_axes):
     for j,run in enumerate(sim_ylabels):
 	mean = np.mean(sim_tables[j][xdata_labels[i]])
 	stdev = np.std(tables[j][xdata_labels[i]])
-        sax.errorbar([mean],[j],xerr=[stdev],capsize=8.0,markersize=10,marker='o',
+        error = stdev#/len(tables[j]**0.5)
+	sax.errorbar([mean],[j],xerr=[error],capsize=8.0,markersize=10,marker='o',
                     color=colors[j%(divisions[1]-divisions[0])])
 
         if j%(divisions[1]-divisions[0])==0:
